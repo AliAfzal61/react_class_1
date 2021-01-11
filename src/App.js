f406760ffb802217e1e3e5b1e161f5496e46177b
Ali Afzal <aliafzal6111@gmail.com>
2021-01-12 01:26:18 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return <div>This is for testing on surge</div>
+  return <div>This is for testing on surge..</div>
 }
 
 export default App;
